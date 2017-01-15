8677b81701540f3bf7b72175866fe46041093af1
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:35:40 +1100

    GIA 2i1a - redistributeStanfordRelationsInterpretNamedAsDefinition: upgrade GIA to assign alias for "called x"

--- GIA/GIAglobalDefs.h
+++ GIA/GIAglobalDefs.h
@@ -26,7 +26,7 @@
  * File Name: GIAglobalsDefs.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2h4a 02-December-2014
+ * Project Version: 2h5a 13-January-2015
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  * Description: GIA specific global definitions
  *
