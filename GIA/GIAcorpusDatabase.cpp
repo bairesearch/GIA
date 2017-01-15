8de597a77d07157c08e8a3b71c5579159a2c5319
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:36:29 +1100

    GIA 2i25b - update redistributeStanfordRelationsInterpretNamedAsDefinition2 to determine sameReferenceSet based on existence of _auxpass

--- GIA/GIAcorpusDatabase.cpp
+++ GIA/GIAcorpusDatabase.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAcorpusDatabase.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2i25a 04-February-2015
+ * Project Version: 2i25b 04-February-2015
  * Requirements: requires text parsed by GIA2 Parser (Modified Stanford Parser format)
  *
  *******************************************************************************/
--- GIA/GIAmain.cpp
+++ GIA/GIAmain.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAmain.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2i25a 04-February-2015
+ * Project Version: 2i25b 04-February-2015
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  *
  *******************************************************************************/
@@ -634,7 +634,7 @@ int main(int argc,char* *argv)
 
 		if (argumentExists(argc,argv,"-version"))
 		{
-			cout << "OpenGIA.exe - Project Version: 2i25a 04-February-2015" << endl;
+			cout << "OpenGIA.exe - Project Version: 2i25b 04-February-2015" << endl;
 			exit(1);
 		}
 
