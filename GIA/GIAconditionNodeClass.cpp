8f5edaa6ef4ba1b0eba7e60a8d2da729e40cfa1b
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:31:44 +1100

    GIA 1u4a - when finding advanced references, if matched items are substance concepts, ignore plurality (in fact substance concepts should not be assigned plural in the first place; this is an artefact of the english grammmar system: eg "blue chickens are strong")

--- GIA/GIAconditionNodeClass.cpp
+++ GIA/GIAconditionNodeClass.cpp
@@ -23,7 +23,7 @@
  * File Name: GIAconditionNodeClass.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2013 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 1u3c 29-Sept-2013
+ * Project Version: 1u4a 29-Sept-2013
  *
  *******************************************************************************/
 
--- GIA/GIAcxlConversion.h
+++ GIA/GIAcxlConversion.h
@@ -23,7 +23,7 @@
  * File Name: GIAcxlConversion.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2013 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 1u3c 29-Sept-2013
+ * Project Version: 1u4a 29-Sept-2013
  * Description: Converts GIA network nodes into an XML, or converts an XML file into GIA network nodes
  *
  *******************************************************************************/
