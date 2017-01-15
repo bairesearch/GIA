99e933b526da55ddf130d8397b23bc2b9cbf3400
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:35:16 +1100

    GIA 2g13a - GIA_TRANSLATOR_MARK_DOUBLE_LINKS_AS_REFERENCE_CONNECTIONS: update writeVectorConnection() to not vectorConnection->clear() - fix issue with GIA; !sameReferenceSet connections are losing their bidirection after advanced referencing

--- GIA/GIAcorpus.cpp
+++ GIA/GIAcorpus.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAcorpus.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2g14a 05-November-2014
+ * Project Version: 2g13a 04-November-2014
  * Requirements: requires text parsed by GIA2 Parser (Modified Stanford Parser format)
  *
  *******************************************************************************/
--- GIA/GIAnlpParser.h
+++ GIA/GIAnlpParser.h
@@ -26,7 +26,7 @@
  * File Name: GIAnlpParser.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2g14a 05-November-2014
+ * Project Version: 2g13a 04-November-2014
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  * Description: Parses tabular subsections (Eg <relations>) of RelEx CFF/Stanford Parser File
  *
