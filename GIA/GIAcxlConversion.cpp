b39e6409e7c9460704fc4aa158dd07bd5cd819f2
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:31:40 +1100

    GIA 1u1c - implement 'with' as property

--- GIA/GIAcxlConversion.cpp
+++ GIA/GIAcxlConversion.cpp
@@ -23,7 +23,7 @@
  * File Name: GIAcxlConversion.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2013 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 1t9b 15-Sept-2013
+ * Project Version: 1q1c 15-Sept-2013
  * Description: Converts GIA network nodes into an XML, or converts an XML file into GIA network nodes
  * NB this function creates entity idActiveListReorderdIDforXMLsave values upon write to speed up linking process (does not use original idActiveList values)
  *
--- GIA/GIAwordnet.cpp
+++ GIA/GIAwordnet.cpp
@@ -23,7 +23,7 @@
  * File Name: GIAwordnet.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2013 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 1t9b 15-Sept-2013
+ * Project Version: 1q1c 15-Sept-2013
  * Requirements: requires wordnet libraries to be installed
  * Description: searches wordnet database and parses wordnet output
  *
