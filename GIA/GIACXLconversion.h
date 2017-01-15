b6906f280f4beb69b6bee6cf07c355bb7c83f739
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:30:20 +1100

    GIA 1l4d - fix up display of finish time (month+1) GIA 1l4d - reconfigure semantic network warnings in BAI App GIA + reconfigure input parameter warning for BAI App Server Compilation;

--- GIA/GIACXLconversion.h
+++ GIA/GIACXLconversion.h
@@ -3,7 +3,7 @@
  * File Name: GIACXLconversion.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2012 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 1l4c 02-June-2012
+ * Project Version: 1l4d 02-June-2012
  * Description: Converts GIA network nodes into an XML, or converts an XML file into GIA network nodes
  *
  *******************************************************************************/
--- GIA/GIAParser.cpp
+++ GIA/GIAParser.cpp
@@ -3,7 +3,7 @@
  * File Name: GIAParser.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2012 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 1l4c 02-June-2012
+ * Project Version: 1l4d 02-June-2012
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  * Description: Parses tabular subsections (Eg <relations>) of CFF File
  *
