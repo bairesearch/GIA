c1d567130ea57a1fb01f75108b2854c7654a5b2f
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:34:08 +1100

    GIA 2g2b - no changes

--- GIA/GIAcorpusTranslator.cpp
+++ GIA/GIAcorpusTranslator.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAcorpusTranslator.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2g2a 29-August-2014
+ * Project Version: 2g2b 29-August-2014
  * Requirements: requires text parsed by GIA2 Parser (Modified Stanford Parser format)
  *
  *******************************************************************************/
