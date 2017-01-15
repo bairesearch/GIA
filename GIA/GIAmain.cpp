d08fd2c43c7b1fe270851425270bfe9e5da33f3b
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:34:31 +1100

    GIA 2g8a - fix GIAdraw.cpp bug; maxNumberOfSentences; take "maxNumberSentences = 0" initialisation out of preprocessor def

--- GIA/GIAmain.cpp
+++ GIA/GIAmain.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAmain.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2g7a 10-September-2014
+ * Project Version: 2g8a 13-September-2014
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  *
  *******************************************************************************/
@@ -639,7 +639,7 @@ int main(int argc,char **argv)
 
 		if (argumentExists(argc,argv,"-version"))
 		{
-			cout << "OpenGIA.exe - Project Version: 2g7a 10-September-2014" << endl;
+			cout << "OpenGIA.exe - Project Version: 2g8a 13-September-2014" << endl;
 			exit(1);
 		}
 
@@ -1010,9 +1010,7 @@ bool executeGIA2()
 
 	bool result = true;
 
-	#ifdef GIA_RECORD_WAS_REFERENCE_INFORMATION
 	*maxNumberSentences = 0;		//variable only used with GIA_RECORD_WAS_REFERENCE_INFORMATION
-	#endif
 
 
 	#ifdef GIA_MAIN_DEBUG
