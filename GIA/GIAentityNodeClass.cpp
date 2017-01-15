6a8b420ed953c6b5dfd15a389b9c9c721fbaaf10
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:33:10 +1100

    GIA 2f12b - update redistributeStanfordRelationsDisablePredeterminers() to support _det and _predet

--- GIA/GIAentityNodeClass.cpp
+++ GIA/GIAentityNodeClass.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAentityNodeClass.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2f12a 13-July-2014
+ * Project Version: 2f12b 14-July-2014
  *
  *******************************************************************************/
 
