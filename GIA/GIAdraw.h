5b5591c66596c5ab65734ebf4785417b2d93010e
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:37:25 +1100

    GIA 2k5c - GIA_CREATE_NON_SPECIFIC_SUBSTANCE_CONCEPTS_FOR_ALL_CONCEPTS: remove all redundant substance definition functions from GIArules.xml;

--- GIA/GIAdraw.h
+++ GIA/GIAdraw.h
@@ -26,7 +26,7 @@
  * File Name: GIAdraw.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2k5b 21-July-2015
+ * Project Version: 2k5c 21-July-2015
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  * Description: Draws GIA nodes in GIA network/tree
  *
