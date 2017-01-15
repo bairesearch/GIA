f68c34e80b42ea2b87cf78fd1639c4704fa30431
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:31:56 +1100

    GIA 2a1b change linkDependentActionsType2 dep(create-6, copy-2) from "requires" to "involves"

--- GIA/GIAtranslatorOperations.h
+++ GIA/GIAtranslatorOperations.h
@@ -23,7 +23,7 @@
  * File Name: GIAtranslatorOperations.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2013 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2a1a 27-October-2013
+ * Project Version: 2a1b 27-October-2013
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  * Description: Converts relation objects into GIA network nodes (of type entity, action, condition etc) in GIA network/tree
  *
@@ -52,7 +52,7 @@ using namespace std;
 
 #define GIA_TRANSLATOR_LINK_DEPENDENT_ACTIONS_TYPE1			//added 30 June 2013 - 1s7f
 #define GIA_TRANSLATOR_LINK_DEPENDENT_ACTIONS_TYPE2			//added 29 June 2013 - 1s7c / modified [to condition] 30 June 2013 - 1s7f
-#define GIA_TRANSLATOR_LINK_DEPENDENT_ACTIONS_DEFAULT_CONDITION_NAME "requires"
+#define GIA_TRANSLATOR_LINK_DEPENDENT_ACTIONS_DEFAULT_CONDITION_NAME "involves"
 #define GIA_REDISTRIBUTE_STANFORD_RELATIONS_DEP_AND_PREP		//added 30 June 2013 - 1s7f
 
 
--- GIA/GIAxmlConversion.h
+++ GIA/GIAxmlConversion.h
@@ -23,7 +23,7 @@
  * File Name: GIAxmlConversion.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2013 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2a1a 27-October-2013
+ * Project Version: 2a1b 27-October-2013
  * Description: Converts GIA network nodes into an XML, or converts an XML file into GIA network nodes
  *
  *******************************************************************************/
