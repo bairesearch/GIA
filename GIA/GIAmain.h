c48e7fca4ec8e43a49014a0c795cc57d80721275
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:36:28 +1100

    GIA 2i25a - split redistributeStanfordRelationsInterpretNamedAsDefinition from redistributeStanfordRelationsInterpretNamedAsDefinition2

--- GIA/GIAmain.h
+++ GIA/GIAmain.h
@@ -26,7 +26,7 @@
  * File Name: GIAmain.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2i24b 03-February-2015
+ * Project Version: 2i25a 04-February-2015
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  *
  *******************************************************************************/
--- GIA/GIAxmlConversion.cpp
+++ GIA/GIAxmlConversion.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAxmlConversion.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2i24b 03-February-2015
+ * Project Version: 2i25a 04-February-2015
  * Description: Converts GIA network nodes into an XML, or converts an XML file into GIA network nodes
  * NB this function creates entity idActiveListReorderdIDforXMLsave values upon write to speed up linking process (does not use original idActiveList values)
  * NB this function creates entity idActiveList values upon read (it could create idActiveListReorderdIDforXMLsave values instead - however currently it is assumed that when an XML file is loaded, this will populate the idActiveList in its entirety)
