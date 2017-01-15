ea173b84f5f40d05511fae322f9222166c8a5b43
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:40:00 +1100

    GIA 2n6a - upgrade redistributeStanfordRelationsPredeterminers to support dep(have-4, each-3) / nsubj(have-4, players-2)

--- GIA/GIAsemanticParser.cpp
+++ GIA/GIAsemanticParser.cpp
@@ -21,7 +21,7 @@
  * File Name: GIAsemanticParser.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2016 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2n5a 30-September-2016
+ * Project Version: 2n6a 02-October-2016
  * Requirements: requires text parsed by GIA2 Parser (Modified Stanford Parser format)
  *
  *******************************************************************************/
