47e8f87d8c9eec474550754abb391f0ccd09ff96
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:40:24 +1100

    GIA 2p1c - replace all GIA_DISABLE_2([a-zA-Z])_CODE_FOR_DEBUG with GIA_DEBUG_DISABLE_1\1_CODE

--- GIA/GIAtranslatorLinkEntitiesDynamic.cpp
+++ GIA/GIAtranslatorLinkEntitiesDynamic.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAtranslatorLinkEntitiesDynamic.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2016 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2p1b 08-December-2016
+ * Project Version: 2p1c 08-December-2016
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  * Description: Converts relation objects into GIA nodes (of type entity, action, condition etc) in GIA network/tree
  *
