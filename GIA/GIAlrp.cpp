2f815e406a207ff7f499339afc093fda221a4732
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:34:54 +1100

    GIA 2g11c - !GIA_DISABLE_CROSS_SENTENCE_REFERENCING compilation compatibility update

--- GIA/GIAlrp.cpp
+++ GIA/GIAlrp.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAlrp.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2g11b 21-October-2014
+ * Project Version: 2g11c 21-October-2014
  * Requirements: requires plain text file
  * Description: Language Reduction Preprocessor
  *
--- GIA/GIAtranslatorLinkEntities.cpp
+++ GIA/GIAtranslatorLinkEntities.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAtranslatorLinkEntities.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2g11b 21-October-2014
+ * Project Version: 2g11c 21-October-2014
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  * Description: Converts relation objects into GIA nodes (of type entity, action, condition etc) in GIA network/tree
  *
