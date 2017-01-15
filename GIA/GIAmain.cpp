77e8784ae89ba8df00ac6c730c97b13b4f25da1e
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:39:13 +1100

    GIA 2m1d - GIA_TRANSLATOR_INTERPRET_PRENOMINAL_MODIFIER_SUBCLASSES: create redistributeStanfordRelationsDetectSubclasses

--- GIA/GIAmain.cpp
+++ GIA/GIAmain.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAmain.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2016 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2m1c 31-August-2016
+ * Project Version: 2m1d 31-August-2016
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  *
  *******************************************************************************/
@@ -623,7 +623,7 @@ int main(int argc, char** argv)
 
 		if(argumentExists(argc, argv, "-version"))
 		{
-			cout << "OpenGIA.exe - Project Version: 2m1c 31-August-2016" << endl;
+			cout << "OpenGIA.exe - Project Version: 2m1d 31-August-2016" << endl;
 			exit(1);
 		}
 
--- GIA/GIAsemanticParserOperations.h
+++ GIA/GIAsemanticParserOperations.h
@@ -21,7 +21,7 @@
  * File Name: GIAsemanticParserOperations.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2016 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2m1c 31-August-2016
+ * Project Version: 2m1d 31-August-2016
  * Requirements: requires text parsed by GIA2 Parser (Modified Stanford Parser format)
  *
  *******************************************************************************/
