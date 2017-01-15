3e1648c902b03840a724df881678595c37f229d3
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:38:03 +1100

    GIA 2k8b - ensure all BAI commercial only source files have BAI commercial header (GIAsemanticParser*.cpp/.h)

--- GIA/GIAsemanticParserTranslator.cpp
+++ GIA/GIAsemanticParserTranslator.cpp
@@ -2,22 +2,17 @@
  *
  * This file is part of BAIPROJECT.
  *
- * BAIPROJECT is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License version 3
- * only, as published by the Free Software Foundation. The use of
- * intermediary programs or interfaces including file i/o is considered
- * remote network interaction. This does not imply such arrangements
- * do not constitute derivative works.
+ * BAIPROJECT is commercially licensed software. It may not be redistributed,
+ * and may only be used by a registered licensee.
  *
- * BAIPROJECT is distributed in the hope that it will be useful,
+ * BAIPROJECT is sold in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License version 3 for more details
+ * BAI Commercial License for more details
  * (a copy is included in the LICENSE file that accompanied this code).
  *
- * You should have received a copy of the GNU Affero General Public License
- * version 3 along with BAIPROJECT.  If not, see <http://www.gnu.org/licenses/>
- * for a copy of the AGPLv3 License.
+ * You will have received a copy of the BAI Commercial License
+ * along with BAIPROJECT. For more information, visit <http://www.baxterai.com>
  *
  *******************************************************************************/
 
@@ -26,7 +21,7 @@
  * File Name: GIAsemanticParserTranslator.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2k8a 06-September-2015
+ * Project Version: 2k8b 06-September-2015
  * Requirements: requires text parsed by GIA2 Parser (Modified Stanford Parser format)
  *
  *******************************************************************************/
--- GIA/GIAsentenceClass.cpp
+++ GIA/GIAsentenceClass.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAsentenceClass.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2k8a 06-September-2015
+ * Project Version: 2k8b 06-September-2015
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  *
  *******************************************************************************/
