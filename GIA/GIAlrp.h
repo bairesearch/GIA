06bcc0ae231073cde8c360ba6b1be0b0baf821fa
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:35:53 +1100

    GIA 2i8d - update redistributeStanfordRelationsCollapseAdvmodRelationGovernorBe() to take into account rcmod (restore useRedistributeSpecialCaseAuxiliaryIndicatesDifferentReferenceSetCheck code)

--- GIA/GIAlrp.h
+++ GIA/GIAlrp.h
@@ -26,7 +26,7 @@
  * File Name: GIAlrp.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2h12c 21-January-2015
+ * Project Version: 2h12d 21-January-2015
  * Requirements: requires plain text file
  * Description: Language Reduction Preprocessor
  *
