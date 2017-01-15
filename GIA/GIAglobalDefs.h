352023c603fb033e33dcf6ead469986c17ac44d1
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:36:40 +1100

    GIA 2i33a - !GIA_TRANSLATOR_TRANSFORM_THE_ACTION_OF_POSSESSION_EG_HAVING_INTO_A_PROPERTY_BASIC: update interpretWithAsPropertyWhenConnectedToSubstanceOnly()

--- GIA/GIAglobalDefs.h
+++ GIA/GIAglobalDefs.h
@@ -26,7 +26,7 @@
  * File Name: GIAglobalsDefs.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2i32a 11-February-2015
+ * Project Version: 2i33a 13-February-2015
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  * Description: GIA specific global definitions
  *
