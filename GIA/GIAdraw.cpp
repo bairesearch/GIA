2efe19bbf9f441b0b23ffbd9b79e0b60517f7052
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:29:40 +1100

    NO CHANGE RECORD

--- GIA/GIAdraw.cpp
+++ GIA/GIAdraw.cpp
@@ -3,7 +3,7 @@
  * File Name: GIAdraw.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2011 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 1b7a 01-Sept-11
+ * Project Version: 1b8b 22-Sept-11
  * Requirements: requires text parsed by RelEx (available in .CFF format <relations>)
  * Description: Converts relation objects into GIA nodes (of type entity, action, condition etc) in GIA network/tree
  *
