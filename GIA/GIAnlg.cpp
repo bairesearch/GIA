68ac2cddbde2ca1afdeacaddc05144006c327abb
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:37:10 +1100

    GIA 2j12a - GIA_STANFORD_PARSER_AND_CORENLP_VERSION_2015_04_20_OR_GREATER: update relexVersusStanfordDependencyRelations[] to support STANFORD_RELATION_TYPE_PHRASAL_VERB_PARTICLE2->RELATION_TYPE_PHRASAL_VERB_PARTICLE (change compound:prt -> _prt)

--- GIA/GIAnlg.cpp
+++ GIA/GIAnlg.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAnlg.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2j11b 04-July-2015
+ * Project Version: 2j12a 05-July-2015
  * Requirements: requires GIA translated data, and NLG2 to be installed
  * Description: GIA natural language generation (using NLG2)
  *
