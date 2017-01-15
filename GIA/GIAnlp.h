24f2ca46f9a251e862170f1db72afe1295405c24
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:35:37 +1100

    GIA 2h4a - restore USE_NLC: GIA_DISABLE_CROSS_SENTENCE_REFERENCING

--- GIA/GIAnlp.h
+++ GIA/GIAnlp.h
@@ -26,7 +26,7 @@
  * File Name: GIAnlp.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2h3a 30-November-2014
+ * Project Version: 2h4a 02-December-2014
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  *
  *******************************************************************************/
--- GIA/GIAwordnet.cpp
+++ GIA/GIAwordnet.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAwordnet.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 2h3a 30-November-2014
+ * Project Version: 2h4a 02-December-2014
  * Requirements: requires wordnet libraries to be installed
  * Description: searches wordnet database and parses wordnet output
  *
