3118d60db4430d20822f5cd2f897d0c52830e8f4
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:29:41 +1100

    NO CHANGE RECORD

--- GIA/GIASentenceClass.h
+++ GIA/GIASentenceClass.h
@@ -3,7 +3,7 @@
  * File Name: GIASentenceClass.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2011 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 1b8b 22-Sept-11
+ * Project Version: 1b10b 28-Sept-11
  * Requirements: requires text parsed by RelEx (available in .CFF format <relations>)
  *
  *******************************************************************************/
