f75a67403a6d450befd695f57b93937a641b2553
Richard Baxter <richard.baxter@baxterai.com>
2017-06-16 13:16:55 +1000

    GIA 3c3a - USE_GIAI: upgrade GIAtranslatorVariablesClass to support giaQueryAnswer

--- GIA/GIAtranslatorOperations.hpp
+++ GIA/GIAtranslatorOperations.hpp
@@ -25,7 +25,7 @@
  * File Name: GIAtranslatorOperations.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2017 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3c2b 12-June-2017
+ * Project Version: 3c3a 16-June-2017
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  * Description: Converts relation objects into GIA network nodes (of type entity, action, condition etc) in GIA network/tree
  *
@@ -116,7 +116,10 @@ public:
 	#ifdef USE_NLCI
 	string nlcGeneratedCode;
 	#endif
-	
+	#ifdef USE_GIAI
+	string giaQueryAnswer;
+	#endif
+		
 	#ifdef USE_CE	
 	CECodeextension* firstCodeextensionInHeirachy;
 	vector<CECodeextension*>* codeextensionsList;
