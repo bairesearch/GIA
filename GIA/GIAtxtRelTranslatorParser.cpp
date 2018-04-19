f272543db518698abb6bd390e6527e824c3dff0b
Richard Baxter <richard.baxter@baxterai.com>
2018-04-20 02:09:10 +1000

    GIA 3f7b - do not disable entity if isExpletive GIA 3f7b - update referenceSets:explitives to use determinerBasicIndefinite (support an) rather than explicit word="a"

--- GIA/GIAtxtRelTranslatorParser.cpp
+++ GIA/GIAtxtRelTranslatorParser.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAtxtRelTranslatorParser.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2018 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3f7a 17-April-2018
+ * Project Version: 3f7b 17-April-2018
  * Requirements: 
  * Description: Textual Relation Translator Parser
  * /
@@ -170,6 +170,12 @@ bool GIAtxtRelTranslatorParserClass::convertSentenceTxtRelationsIntoGIAnetworkNo
 		{
 			disableEntity = false;
 		}
+		#ifdef GIA_TXT_REL_TRANSLATOR_RULES_CODE_NUMBER_EXPLETIVES
+		if(entity->isExpletive)
+		{
+			disableEntity = false;
+		}
+		#endif
 		if(disableEntity)
 		{
 			#ifdef GIA_DEBUG_TXT_REL_TRANSLATOR_RULES_PRINT_PARSE_PROCESS2
