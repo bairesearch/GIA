889a44e36c4eaac35374df714e9e2e1c7f93c681
Richard Baxter <richard.baxter@baxterai.com>
2018-04-20 02:09:17 +1000

    GIA 3f8d - fix bug in GIAtxtRelTranslatorClass::executeTxtRelTranslator introduced by @NLC3f8a currentWord->POStypeInferred restoration implementation; save robust version of POStypeInferred in parse tree components, then transfer to word objects after finished parse

--- GIA/GIAxmlConversion.hpp
+++ GIA/GIAxmlConversion.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAxmlConversion.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2018 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3f8c 18-April-2018
+ * Project Version: 3f8d 18-April-2018
  * Description: XML Conversion - Converts GIA network nodes into an XML file, or converts an XML file into GIA network nodes
  * /
  *******************************************************************************/
