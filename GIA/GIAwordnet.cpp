3eab98a365442fd5bbfae0295037c6634f082718
Richard Baxter <richard.baxter@baxterai.com>
2018-04-20 02:08:28 +1000

    GIA 3f1q - restore wordlistPronounDemonstrative.txt to wordlists.txt GIA 3f1q - update subReferenceSetsSubject with group pronounDemonstrative: case wordlists.txt GIA 3f1q - create auxiliarySequenceBeingOrHaving and update subReferenceSetsAuxiliaryDelimiterAndObject:actionReverse to use this instead of auxiliarySequence, and check actionReverse before action GIA 3f1q - [CHECKTHIS] update GIAtxtRelTranslatorClass::generateRulesGroupTreeComponents to check subcomponentsOptional for <or> subcomponents not just <repeat> subcomponents GIA 3f1q - end test GIA_USE_ADVANCED_REFERENCING - some 'that' usages [with GIA_TXT_REL_TRANSLATOR]

--- GIA/GIAwordnet.cpp
+++ GIA/GIAwordnet.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAwordnet.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2018 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3f1p 22-February-2018
+ * Project Version: 3f1q 22-February-2018
  * Requirements: requires wordnet libraries to be installed
  * Description: Wordnet - searches wordnet database and parses wordnet output
  * /
