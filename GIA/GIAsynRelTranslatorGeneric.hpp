aa71b0b7fb3338a972e64ecbafd7a29b319d4d82
Richard Baxter <richard.baxter@baxterai.com>
2018-04-20 02:08:32 +1000

    GIA 3f1u - partially revert change introduced @GIA3f1q; remove "update subReferenceSetsAuxiliaryDelimiterAndObject:actionReverse to use this instead of auxiliarySequence" from "create auxiliarySequenceBeingOrHaving and update subReferenceSetsAuxiliaryDelimiterAndObject:actionReverse to use this instead of auxiliarySequence, and check actionReverse before action" GIA 3f1u - revert change introduced @GIA3f1s; "[CHECKTHIS] update subReferenceSetsAuxiliaryDelimiterAndObject:definition and subReferenceSetsDelimiterAndObject:definition to use subReferenceSetsSubject instead of subReferenceSetsSpecialPossessiveOrDeterminerThingEnforced"

--- GIA/GIAsynRelTranslatorGeneric.hpp
+++ GIA/GIAsynRelTranslatorGeneric.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAsynRelTranslatorGeneric.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2018 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3f1t 22-February-2018
+ * Project Version: 3f1u 22-February-2018
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  * Description: Syntactic Relation Translator - Converts relation objects into GIA nodes (of type entity, action, condition etc) in GIA network/tree
  * /
