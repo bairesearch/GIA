1eba3f37f97633e7deac825822c9808abc741f92
Richard Baxter <richard.baxter@baxterai.com>
2018-04-20 02:09:20 +1000

    GIA 3f9b - sync with NLC2f9b

--- GIA/GIApreprocessorWordClass.cpp
+++ GIA/GIApreprocessorWordClass.cpp
@@ -26,7 +26,7 @@
  * File Name: GIApreprocessorWordClass.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2018 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3f9a 18-April-2018
+ * Project Version: 3f9b 18-April-2018
  * Requirements: requires plain text file
  * Description: Preprocessor Word Class
  * /
@@ -585,6 +585,9 @@ bool GIApreprocessorWordClassClass::findAndReplaceSimpleSubstringInWordListAtInd
 	generateSentenceWordListFromStringSimple(&wordListToFind, &stringSimpleToFind);
 	vector<GIApreprocessorPlainTextWord*> wordListReplacement;
 	generateSentenceWordListFromStringSimple(&wordListReplacement, &stringSimpleReplacement);
+	
+	//cout << "stringSimpleToFind = " << stringSimpleToFind << endl;
+	//cout << "stringSimpleReplacement = " << stringSimpleReplacement << endl;
 	
 	if(findAndReplaceWordListInWordListAtIndexWithWordList(wordList, &wordListToFind, indexToPerformFind, &wordListReplacement))
 	{
