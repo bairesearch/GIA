365eef406ba27b086a3cf8d5870de38d35a0f959
Richard Baxter <richard.baxter@baxterai.com>
2018-04-20 02:08:43 +1000

    GIA 3f2k - [CHECKTHIS] upgrade logicReferenceSets preposition1/preposition2 to detect all prepositions (not just those from tokens)? GIA 3f2k - update conditionAndConclusion2 to support an intermediate comma (optional)

--- GIA/GIAentityConnectionClass.hpp
+++ GIA/GIAentityConnectionClass.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAentityConnectionClass.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2018 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3f2j 04-April-2018
+ * Project Version: 3f2k 04-April-2018
  * NB a substance is an instance of an entity, any given entity may contain/comprise/have multiple substances - and substances are unrelated to definitions between entities [they just define what comprises any given entity]
  * /
  *******************************************************************************/
