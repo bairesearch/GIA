39f503a5ec7deabfd8ccfd1110e9d3a52cf27cf6
Richard Baxter <richard.baxter@baxterai.com>
2018-04-20 02:09:11 +1000

    GIA 3f7c - remove "the city" from wordlistNoun.txt/multiwordlistNoun.txt

--- GIA/GIAsemRelTranslatorOperations.hpp
+++ GIA/GIAsemRelTranslatorOperations.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAsemRelTranslatorOperations.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2018 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3f7b 17-April-2018
+ * Project Version: 3f7c 17-April-2018
  * Requirements: requires text parsed by GIA2 Parser (Modified Stanford Parser format)
  * Description: Semantic Relation Translator
  * /
