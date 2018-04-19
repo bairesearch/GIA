649fbdfa12ad422f6e2c9d0b5ae96467d670491f
Richard Baxter <richard.baxter@baxterai.com>
2018-04-20 02:08:58 +1000

    GIA 3f3m - remove not from wordlistsAdverb.txt GIA 3f3m - add not from wordlistsUnknown.txt (to prevent it from being interpreted as a wildcard/noun) GIA 3f3m - create auxiliarySequence3/auxiliarySequenceHaving3/auxiliarySequenceBeing3/auxiliarySequenceDoing3 to prevent auxiliary sequences from starting with 'not'

--- GIA/GIAnlg.hpp
+++ GIA/GIAnlg.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAnlg.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2018 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3f3l 10-April-2018
+ * Project Version: 3f3m 10-April-2018
  * Requirements: requires GIA translated data, and NLG2 to be installed
  * Description: NLG - natural language generation
  * /
