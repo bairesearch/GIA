2764432b90606f251e4f4529815dd31b75c3fad2
Richard Baxter <richard.baxter@baxterai.com>
2019-01-05 22:02:13 +1100

    GIA 3g8h - GIA_TXT_REL_TRANSLATOR_NEURAL_NETWORK_TAKE_LAST_SUCCESSFUL_PARSE - disable forwardPropogationSentenceData->finishedPassingSentenceWords = true; GIA 3g8h - [CHECKTHIS] GIA_TXT_REL_TRANSLATOR_NEURAL_NETWORK_TAKE_LAST_SUCCESSFUL_PARSE - always take successfully parses that require the least number of iterations - maxIterationIndex = true;

--- GIA/GIAsemRelTranslator.hpp
+++ GIA/GIAsemRelTranslator.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAsemRelTranslator.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2018 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3g8g 19-December-2018
+ * Project Version: 3g8h 19-December-2018
  * Requirements: requires text parsed by GIA2 Parser (Modified Stanford Parser format)
  * Description: Semantic Relation Translator
  * /
--- GIA/GIAwordnet.hpp
+++ GIA/GIAwordnet.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAwordnet.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2018 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3g8g 19-December-2018
+ * Project Version: 3g8h 19-December-2018
  * Requirements: requires wordnet libraries to be installed
  * Description: Wordnet - searches wordnet database and parses wordnet output
  * /
