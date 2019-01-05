47774634db6e6ba6589c8e74e619f77fc81bf00d
Richard Baxter <richard.baxter@baxterai.com>
2019-01-05 22:01:54 +1100

    GIA 3g1f - update GIAtxtRelTranslatorNeuralNetworkClass::propagateWordThroughNetworkGroupComponent to continuously add new components to parse tree upon activation; add currentParseTreeGroupRef to GIAtxtRelTranslatorRulesComponent GIA 3g1f - remove depreciated option GIA_TXT_REL_TRANSLATOR_NEURAL_NETWORK_BACK_GROUP_REF_REPEAT_COUNTER GIA 3g1f - update GIAtxtRelTranslatorNeuralNetworkClass::propagateWordThroughNetworkGroupComponent to store activationGroupRefs [single entry only] only for standard componentTypes, and activationGroupRefs and repeatDetected componentTypes GIA 3g1f - make GIA_TXT_REL_TRANSLATOR_NEURAL_NETWORK_STORE_MULTIPLE_BACK_GROUP_REFS mandatory (rename to parseTreeGroupRefs activationGroupRefs) GIA 3g1f - make GIA_TXT_REL_TRANSLATOR_NEURAL_NETWORK_PARSE_RECORD_PERFORMANCE dependent on GIA_TXT_REL_TRANSLATOR_NEURAL_NETWORK_SAVE_PARSE_TREE GIA 3g1f - rename traceBackpropActivationPath/printBackpropActivationPath to traceBackpropParseTree/printBackpropParseTree GIA 3g1f - move printBackpropParseTree execution to GIAtxtRelTranslatorNeuralNetworkClass::propagateWordThroughNetworkGroupComponent GIA 3g1f - fix bug in resetNeuronBackprop - parse through activationGroupRefs

--- GIA/GIAquery.hpp
+++ GIA/GIAquery.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAquery.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2018 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3g1e 24-April-2018
+ * Project Version: 3g1f 24-April-2018
  * Requirements: requires a GIA network created for both existing knowledge and the query (question)
  * Description: Query - locates (and tags for highlighting) a given query GIA network (subnet) within a larger GIA network of existing knowledge, and identifies the exact answer if applicable (if a comparison variable has been defined within the GIA query network)
  * /
