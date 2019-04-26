a206827b1c2e56ae97ab00d7ad41c883a388c8d9
Richard Baxter <richard.baxter@baxterai.com>
2019-04-27 00:49:55 +1000

    GIA 3g11q - move resetAllowed from GIAtxtRelTranslatorNeuralNetworkOperations to GIAtxtRelTranslatorNeuralNetworkLightOptimisedClass GIA 3g11q - move verifyNewActivationParseTreeContainsExistingActivationWordsInParseTree from GIAtxtRelTranslatorNeuralNetworkOperations to GIAtxtRelTranslatorNeuralNetworkLightOptimisedClass

--- GIA/GIAnlp.cpp
+++ GIA/GIAnlp.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAnlp.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2019 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3g11p 01-March-2019
+ * Project Version: 3g11q 01-March-2019
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  * Description: NLP - external natural language processing
  * /
