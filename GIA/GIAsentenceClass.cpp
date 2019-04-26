1f176f712c1f56fcd36fe58c1b90450186bea33e
Richard Baxter <richard.baxter@baxterai.com>
2019-04-27 00:49:56 +1000

    GIA 3h2a - GIA_TXT_REL_TRANSLATOR_NEURAL_NETWORK_LIGHT_OPTIMISED_BIO_DO_NOT_RELY_ON_PARSE_TREE_MEMORY - use GIAtxtRelTranslatorRulesGroup:parseTreeMinWordIndex/parseTreeMaxWordIndex GIA 3h2a - activate GIA_TXT_REL_TRANSLATOR_NEURAL_NETWORK_ENFORCE_WORD_CONNECTIVITY_BETWEEN_PREVIOUS_ACTIVE_COMPONENTS_AND_NEWLY_ACTIVATED_COMPONENT_FIX_BUG2 (and disable FIX_BUG1); should produce identical output

--- GIA/GIAsentenceClass.cpp
+++ GIA/GIAsentenceClass.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAsentenceClass.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2019 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3h1a 20-April-2019
+ * Project Version: 3h2a 22-April-2019
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  * Description: Sentence Class
  * /
