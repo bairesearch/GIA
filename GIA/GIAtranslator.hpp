987a01c637481c593061c6bc4dff16586fae12fb
Richard Baxter <richard.baxter@baxterai.com>
2019-07-19 21:12:18 +1000

    GIA 3i3b - GIA_TXT_REL_TRANSLATOR_NEURAL_NETWORK_REMOVE_LAST_OPTIONAL_COMPONENTS_OR_OPTIONAL_REPEAT upgrade implementation of GIA_TXT_REL_TRANSLATOR_NEURAL_NETWORK_REMOVE_LAST_OPTIONAL_COMPONENTS to support <repeat optional="true"> GIA 3i3b - update GIAtxtRelTranslatorNeuralNetworkOperationsClass::componentWordConnectivityTestsWrapper; declare GIAtxtRelTranslatorRulesGroupParseTree* previousActiveComponentInParseTreeParseTreeGroupRefNOTUSED = NULL; and update componentWordConnectivityTests reference

--- GIA/GIAtranslator.hpp
+++ GIA/GIAtranslator.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAtranslator.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2019 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3i3a 24-June-2019
+ * Project Version: 3i3b 24-June-2019
  * Requirements: requires text parsed by X Parser
  * Description: Syntactic Relation Translator - Converts relation objects into GIA nodes (of type entity, action, condition etc) in GIA network/tree
  * /
