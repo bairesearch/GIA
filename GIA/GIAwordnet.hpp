33998be59f9ed8cd83b76b07317b832822429c5b
Richard Baxter <richard.baxter@baxterai.com>
2020-03-19 16:45:05 +1100

    GIA 3k5a - GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_LIMIT_NUM_COMPONENTS_OPTIMISE_FOR_DIVERGENT_CONVERGENT_PATHWAYS - execute faster method of calculating max depth

--- GIA/GIAwordnet.hpp
+++ GIA/GIAwordnet.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAwordnet.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3k4a 17-March-2020
+ * Project Version: 3k5a 18-March-2020
  * Requirements: requires wordnet libraries to be installed
  * Description: Wordnet - searches wordnet database and parses wordnet output
  * /
