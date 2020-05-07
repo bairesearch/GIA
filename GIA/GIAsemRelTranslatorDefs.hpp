f54c78b1ac79c11c95cd2372f715a6a3ead75bd4
Richard Baxter <richard.baxter@baxterai.com>
2020-05-07 13:11:56 +1000

    GIA 3k8a - extract updateActivatedNeuronWithMaxWordIndexCoverage from propagateWordThroughNetworkGroupComponentWrapper/propagateWordThroughNetworkGroupComponent

--- GIA/GIAsemRelTranslatorDefs.hpp
+++ GIA/GIAsemRelTranslatorDefs.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAsemRelTranslatorDefs.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3k7b 02-May-2020
+ * Project Version: 3k8a 06-May-2020
  * Requirements: 
  * Description: Syntactic Relation Translator - Converts relation objects into GIA nodes (of type entity, action, condition etc) in GIA network/tree
  *
