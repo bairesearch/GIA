dec08bb09686fb7a66cb6a1db234f5e4ca793af2
Richard Baxter <richard.baxter@baxterai.com>
2020-05-13 14:21:40 +1000

    GIA 3k11c - extract findAndReconcileVariationWrapper from GIAposRelTranslatorSANIPropagateCompactGenerateClass::executeTxtRelTranslatorNeuralNetwork GIA 3k11c - GIA_POS_REL_TRANSLATOR_SANI_PREVENT_RECORDING_TOP_LEVEL_NEURON_IF_RECORDING_MAX_COVERAGE_NEURON

--- GIA/GIAwordnet.hpp
+++ GIA/GIAwordnet.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAwordnet.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3k11b 09-May-2020
+ * Project Version: 3k11c 09-May-2020
  * Requirements: requires wordnet libraries to be installed
  * Description: Wordnet - searches wordnet database and parses wordnet output
  * /
