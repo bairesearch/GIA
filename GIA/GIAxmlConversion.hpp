cc2affd99151320f7173fb36159eff34a18bed12
Richard Baxter <richard.baxter@baxterai.com>
2020-05-13 14:21:39 +1000

    GIA 3k11b - GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_COMPONENT_SUPPORT_VARIABLE_FIRST_COMPONENTS: extract verifyMissingOrVariableStartComponentFound/verifyMissingStartComponentFound from GIAposRelTranslatorSANIPropagateCompactClass::propagateWordThroughNetworkGroupComponentWrapper GIA 3k11b - GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_EXECUTE_WORD_CONNECTIVITY_TESTS_ONCE: execute sequentialActivationConnectivityTests at start to save multiple executions GIA 3k11b - GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_EXECUTE_WORD_CONNECTIVITY_TESTS_ONCE: update GIAposRelTranslatorSANIPropagateCompactClass::sequentialActivationConnectivityTests to ensure groupHasPreceedingComponent (ie currentComponent->componentIndex > 0) before executing componentWordConnectivityTestsWrapper

--- GIA/GIAxmlConversion.hpp
+++ GIA/GIAxmlConversion.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAxmlConversion.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3k11a 09-May-2020
+ * Project Version: 3k11b 09-May-2020
  * Description: XML Conversion - Converts GIA network nodes into an XML file, or converts an XML file into GIA network nodes
  * /
  *******************************************************************************/
