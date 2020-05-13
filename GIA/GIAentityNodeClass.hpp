0d772878d897476c1d950c80ce1bbf3d801c862b
Richard Baxter <richard.baxter@baxterai.com>
2020-05-13 14:21:47 +1000

    GIA 3k14e - [CHECKTHIS] GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_LIMIT_NUM_COMPONENTS_SUPPORT_VARIABLE_FIRST_COMPONENTS_PREVENT_DUPLICATE_CONNECTIONS - update GIAposRelTranslatorSANIPropagateCompactGenerateClass::addVariableComponentToGroup to verify the connection does not already exist

--- GIA/GIAentityNodeClass.hpp
+++ GIA/GIAentityNodeClass.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAentityNodeClass.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3k14d 12-May-2020
+ * Project Version: 3k14e 12-May-2020
  * NB a substance is an instance of an entity, any given entity may contain/comprise/have multiple substances - and substances are unrelated to definitions between entities [they just define what comprises any given entity]
  * /
  *******************************************************************************/
