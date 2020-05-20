13214a98f0350313ea1376cd7774e03237a6d617
Richard Baxter <richard.baxter@baxterai.com>
2020-05-20 23:39:56 +1000

    GIA 3k15c - upgrade GIAposRelTranslatorSANIPropagateOperationsClass::repeatedSequenceDetected to accept wordTranslatorSentenceWordIndex as input parameter rather than using component->neuronComponentConnectionActiveWordRecord->translatorSentenceWordIndex GIA 3k15c - GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_LIMIT_NUM_COMPONENTS_PREVENT_ACTIVATION_IF_REPEATED_SEQUENCE_MISMATCH_DETECTED: upgrade GIAposRelTranslatorSANIPropagateOperationsClass::propagateWordThroughNetworkGroupVerifyComponentSequenceActivationReady to handle more complex situations in which !findValidDualLowerLevelConnection; GIA 3k15c - GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_LIMIT_NUM_COMPONENTS_PREVENT_ACTIVATION_IF_REPEATED_SEQUENCE_MISMATCH_DETECTED: upgrade GIAposRelTranslatorSANIPropagateOperationsClass::propagateWordThroughNetworkGroupVerifyComponentSequenceActivationReady; if repeated POS instance detected in sentence starting from forwardPropogationWordData, and first section [lowest level input] of second component is not equivalent to same input group as first component, then disallow activation[/reactivation] of first component GIA 3k15c - GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_LIMIT_NUM_COMPONENTS_PREVENT_ACTIVATION_IF_REPEATED_SEQUENCE_MISMATCH_DETECTED: upgrade GIAposRelTranslatorSANIPropagateOperationsClass::propagateWordThroughNetworkGroupVerifyComponentSequenceActivationReady; if repeated POS instance not detected in sentence starting from forwardPropogationWordData, and first section [lowest level input] of second component is equivalent to same input group as first component, then disallow activation[/reactivation] of first component GIA 3k15c - fix bug in GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_LIMIT_NUM_COMPONENTS_PREVENT_RESET_IF_REPEATED_SEQUENCE_DETECTED_ALTERNATE to test repeatedSequenceDetected based on prospective component->groupSource (ie activationPathWordCurrentParseTreeGroup) instead of current component->groupSource GIA 3k15c - fix bug in GIAposRelTranslatorSANIPropagateCompactGenerateClass::createNewGroup - set newGroup->initiateANNneuron("groupIndex:" + SHAREDvars.convertIntToString(newNeuronIndex-1)) GIA 3k15c - update GIA_DEBUG_GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_WORDCONNECTIVITY_VERIFICATION_CONTINUOUS - change condition to if((i>=currentParseTreeGroup->parseTreeMinWordIndex) && (i<=currentParseTreeGroup->parseTreeMaxWordIndex)) performance = performance + 1;

--- GIA/GIAnlp.cpp
+++ GIA/GIAnlp.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAnlp.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3k15b 14-May-2020
+ * Project Version: 3k15c 14-May-2020
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  * Description: NLP - external natural language processing
  * /
