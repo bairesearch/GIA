e7703e3d5ffc8448c8a10b4f381fdb816cc88e6f
Richard Baxter <richard.baxter@baxterai.com>
2020-05-20 23:39:55 +1000

    GIA 3k15b - update GIA_DEBUG_GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_WORDCONNECTIVITY_VERIFICATION_CONTINUOUS - remove if(!(forwardPropogationSentenceData->recordActivatedNeuronWithMaxWordIndexCoverage)) condition GIA 3k15b - update GIA_DEBUG_GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_WORDCONNECTIVITY_VERIFICATION_CONTINUOUS - remove if(currentParseTreeGroup->parseTreeMinWordIndex == 0) condition, and add if(i>=currentParseTreeGroup->parseTreeMinWordIndex) performance = performance + 1; GIA 3k15b - fix bug in GIAposRelTranslatorSANIPropagateOperationsClass::propagateWordThroughNetworkGroupVerifyComponentSequenceActivationReady; #ifdef GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_COMPONENT_SUPPORT_VARIABLE_FIRST_COMPONENTS_MISSING; *missingStartComponentsFound = true; , #else; stillParsingActiveComponents = false; GIA 3k15b - make GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_LIMIT_NUM_COMPONENTS_SUPPORT_VARIABLE_FIRST_COMPONENTS_REQUIRE_SIZEABLE_SUBTREES dependent on GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_ADD_NEW_NEURONS_TO_SYMMETRICAL_TREE GIA 3k15b - update GIAposRelTranslatorSANIPropagateOperationsClass::verifyWordIndexCoverageIntegrity to check if(i>=currentParseTreeGroup->parseTreeMinWordIndex) rather than checking if(currentParseTreeGroup->parseTreeMinWordIndex == 0) GIA 3k15b - get GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_ADD_NEW_NEURONS_TO_ELONGATED_TREE to work again at same state as GIA3k14e before moving onto testing  GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_ADD_NEW_NEURONS_TO_SYMMETRICAL_TREE GIA 3k15b - fix bug in GIAposRelTranslatorSANIPropagateCompactClass::sequentialActivationConnectivityTests; remove GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_COMPONENT_SUPPORT_VARIABLE_FIRST_COMPONENTS_MISSING restriction

--- GIA/GIAsynRelTranslatorApplyAdvancedFeatures.cpp
+++ GIA/GIAsynRelTranslatorApplyAdvancedFeatures.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAsynRelTranslatorApplyAdvancedFeatures.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3k15a 14-May-2020
+ * Project Version: 3k15b 14-May-2020
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  * Description: Syntactic Relation Translator - Converts relation objects into GIA nodes (of type entity, action, condition etc) in GIA network/tree
  * /
--- GIA/GIAwordnet.cpp
+++ GIA/GIAwordnet.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAwordnet.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3k15a 14-May-2020
+ * Project Version: 3k15b 14-May-2020
  * Requirements: requires wordnet libraries to be installed
  * Description: Wordnet - searches wordnet database and parses wordnet output
  * /
