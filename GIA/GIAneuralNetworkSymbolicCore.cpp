ee90861ff3f5f42e6dcc9158bd352d0b8989ca84
Richard Baxter <richard.baxter@baxterai.com>
2020-05-20 23:39:57 +1000

    GIA 3k15d - GIA_POS_REL_TRANSLATOR_RULES_TREAT_UNKNOWN_POSTYPES: fix bug in GIAposRelTranslatorSANIPropagateOperationsClass::repeatedSequenceDetected; if(componentWordArray1[i]->unambiguousPOSindex != wordPOStype2)

--- GIA/GIAneuralNetworkSymbolicCore.cpp
+++ GIA/GIAneuralNetworkSymbolicCore.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAneuralNetworkSymbolicCore.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3k15c 14-May-2020
+ * Project Version: 3k15d 14-May-2020
  * Description: Neural Network - visual representation of GIA contents in prototype biological neural network
  * /
  *******************************************************************************/
--- GIA/GIAtranslatorGrammar.hpp
+++ GIA/GIAtranslatorGrammar.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAtranslatorGrammar.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3k15c 14-May-2020
+ * Project Version: 3k15d 14-May-2020
  * Requirements: requires text parsed by X Parser
  * Description: Syntactic Relation Translator - Converts relation objects into GIA nodes (of type entity, action, condition etc) in GIA network/tree
  * /
