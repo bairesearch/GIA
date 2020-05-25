43a6f66a122b01fffacf13553911917844cb8dd0
Richard Baxter <richard.baxter@baxterai.com>
2020-05-25 11:24:04 +1000

    GIA 3k16a - activate GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_ADD_NEW_NEURONS_TO_SYMMETRICAL_TREE:GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_COMPONENT_SUPPORT_VARIABLE_FIRST_COMPONENTS_MISSING GIA 3k16a - disable GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_LIMIT_NUM_COMPONENTS_PREVENT_ACTIVATION_IF_REPEATED_SEQUENCE_MISMATCH_DETECTED - not required with GIA_POS_REL_TRANSLATOR_SANI_ENFORCE_WORD_CONNECTIVITY_BETWEEN_PREVIOUS_ACTIVE_COMPONENTS_AND_NEWLY_ACTIVATED_COMPONENT_MEMORY GIA 3k16a - update GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_COMPONENT_SUPPORT_VARIABLE_FIRST_COMPONENTS_MISSING: GIAposRelTranslatorSANIPropagateOperationsClass::propagateWordThroughNetworkGroupVerifyComponentSequenceActivationReady setting of missingStartComponentsFound = true; require (forwardPropogationSentenceData->recordActivatedNeuronWithMaxWordIndexCoverageSupportVariableStartComponent) GIA 3k16a - update GIAposRelTranslatorSANIPropagateCompactGenerateClass::connectListOfHighLevelNeuronsToNewNeuronLimitNumComponentsSection; if(currentHighLevelNeuron->activatedNeuronWithMaxWordIndexCoverageVariableStartComponentTemp) set highLevelNeuronPriorArray[lowestLayerNeuronIndex] = grammaticalSentenceNeuronSubHigher; GIA 3k16a - update GIAposRelTranslatorSANIPropagateCompactGenerateClass::connectListOfHighLevelNeuronsToNewNeuronLimitNumComponentsSection; if(currentHighLevelNeuron->activatedNeuronWithMaxWordIndexCoverageVariableStartComponentTemp) only if(completedIdentifyingSentenceHighLevelNeurons), highLevelNeuronPriorArray[lowestLayerNeuronIndex] = NULL, and execute updateHighLevelNeuronHierachy

--- GIA/GIAdraw.cpp
+++ GIA/GIAdraw.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAdraw.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3k15h 14-May-2020
+ * Project Version: 3k16a 25-May-2020
  * Requirements: 
  * Description: Draw - Draws GIA nodes in GIA network/tree
  * /
