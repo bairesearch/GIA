dee70fbc33d2f3e6866e130bdb82cc9a1f08904b
Richard Baxter <richard.baxter@baxterai.com>
2020-05-27 22:06:47 +1000

    GIA 3k17a - GIA_POS_REL_TRANSLATOR_SANI_FORWARD_DIRECTION: GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR:!GIA_POS_REL_TRANSLATOR_SANI_REVERSE_DIRECTION GIA 3k17a - update GIAtxtRelTranslatorNeuralNetworkPropagateCompactGenerateClass::findAndReconcileIncrementalVariation to set recordActivatedNeuronWithMaxWordIndexCoverageSupportVariableStartComponent GIA 3k17a - GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_COMPONENT_SUPPORT_VARIABLE_FIRST_COMPONENTS/GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_LIMIT_NUM_COMPONENTS_SUPPORT_VARIABLE_FIRST_COMPONENTS: update GIAposRelTranslatorSANIPropagateCompactGenerateClass::findAndReconcileIncrementalVariation/findAndReconcileIncrementalVariationLimitNumComponentsSection to set forwardPropogationSentenceData->recordActivatedNeuronWithMaxWordIndexCoverageSupportVariableStartComponent = false; upon finish GIA 3k17a - shift "forwardPropogationSentenceData->recordActivatedNeuronWithMaxWordIndexCoverage = false;" from GIAposRelTranslatorSANIPropagateCompactClass::performPropagationTest to GIAposRelTranslatorSANIPropagateCompactGenerateClass::findAndReconcileIncrementalVariation/findAndReconcileIncrementalVariationLimitNumComponentsSection GIA 3k17a - GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_COMPONENT_SUPPORT_VARIABLE_FIRST_COMPONENTS: update GIAposRelTranslatorSANIPropagateOperationsClass::propagateWordThroughNetworkGroupVerifyComponentSequenceActivationReady; remove redundant "if(!(forwardPropogationSentenceData->recordActivatedNeuronWithMaxWordIndexCoverage/recordActivatedNeuronWithMaxWordIndexCoverageSupportVariableStartComponent)) if(!(*missingStartComponentsFound))" case GIA 3k17a - GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_COMPONENT_SUPPORT_VARIABLE_FIRST_COMPONENTS: update GIAposRelTranslatorSANIPropagateOperationsClass::propagateWordThroughNetworkGroupVerifyComponentSequenceActivationReady; change all if(forwardPropogationSentenceData->recordActivatedNeuronWithMaxWordIndexCoverage tests to if(forwardPropogationSentenceData->recordActivatedNeuronWithMaxWordIndexCoverageSupportVariableStartComponent GIA 3k17a - GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_COMPONENT_SUPPORT_VARIABLE_FIRST_COMPONENTS: update GIAposRelTranslatorSANIPropagateOperationsClass::propagateWordThroughNetworkGroupVerifyComponentSequenceActivationReady; if(!forwardPropogationSentenceData->recordActivatedNeuronWithMaxWordIndexCoverageSupportVariableStartComponent), passMissingStartComponentTests = true; GIA 3k17a - GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_COMPONENT_SUPPORT_VARIABLE_FIRST_COMPONENTS: update GIAposRelTranslatorSANIForwardPropogationSentenceData::GIAposRelTranslatorSANIForwardPropogationSentenceData: initialise recordActivatedNeuronWithMaxWordIndexCoverageSupportVariableStartComponent = false;

--- GIA/GIAmain.cpp
+++ GIA/GIAmain.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAmain.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3k16a 25-May-2020
+ * Project Version: 3k17a 26-May-2020
  * Requirements: 
  * Description: Main
  * /
@@ -669,7 +669,7 @@ int main(const int argc, const char** argv)
 
 	if(SHAREDvarsClass().argumentExists(argc, argv, "-version"))
 	{
-		cout << "GIA.exe - Project Version: 3k16a 25-May-2020" << endl;
+		cout << "GIA.exe - Project Version: 3k17a 26-May-2020" << endl;
 		exit(EXIT_OK);
 	}
 
