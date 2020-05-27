3c1fe4c87e240cf5a047dee478c08ef0dc9be309
Richard Baxter <richard.baxter@baxterai.com>
2020-05-27 22:06:49 +1000

    GIA 3k17c - [partially revert change @GIA3k16a] !GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_LIMIT_NUM_COMPONENTS:!GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_COMPONENT_SUPPORT_OPTIONAL_AND_MULTIPLE_INPUT:!GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_COMPONENT_SUPPORT_VARIABLE_FIRST_COMPONENTS - update GIAposRelTranslatorSANIPropagateOperationsClass::propagateWordThroughNetworkGroupVerifyComponentSequenceActivationReady to restore setting of missingStartComponentsFound = true; if GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_SUPPORT_PARTIAL_SENTENCE_PROPAGATION GIA 3k17c - GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_COMPONENT_SUPPORT_VARIABLE_FIRST_COMPONENTS:!GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_LIMIT_NUM_COMPONENTS_SUPPORT_VARIABLE_FIRST_COMPONENTS: fix bug in GIAposRelTranslatorSANIPropagateCompactGenerateClass::findAndReconcileIncrementalVariation; if(loopIndex > 0), forwardPropogationSentenceData->recordActivatedNeuronWithMaxWordIndexCoverageSupportVariableStartComponent = true;

--- GIA/GIAneuralNetworkOperations.hpp
+++ GIA/GIAneuralNetworkOperations.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAneuralNetworkOperations.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3k17b 26-May-2020
+ * Project Version: 3k17c 26-May-2020
  * Description: Neural Network - visual representation of GIA contents in prototype biological neural network
  * /
  *******************************************************************************/
--- GIA/GIAsynRelTranslatorLinkEntities.hpp
+++ GIA/GIAsynRelTranslatorLinkEntities.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAsynRelTranslatorLinkEntities.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3k17b 26-May-2020
+ * Project Version: 3k17c 26-May-2020
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  * Description: Syntactic Relation Translator - Converts relation objects into GIA nodes (of type entity, action, condition etc) in GIA network/tree
  * /
