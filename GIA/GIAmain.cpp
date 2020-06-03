726891b672267748b31a610aac4fe1ad1bd2eb08
Richard Baxter <richard.baxter@baxterai.com>
2020-06-03 17:08:23 +1000

    GIA 3l3b - [update change GIA3l2a] fix error in preprocess def structure of GIAposRelTranslatorSANIPropagateCompactGenerateClass::markSubNeurons/markSubNeuronsReset

--- GIA/GIAmain.cpp
+++ GIA/GIAmain.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAmain.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3l3a 02-June-2020
+ * Project Version: 3l3b 02-June-2020
  * Requirements: 
  * Description: Main
  * /
@@ -669,7 +669,7 @@ int main(const int argc, const char** argv)
 
 	if(SHAREDvarsClass().argumentExists(argc, argv, "-version"))
 	{
-		cout << "GIA.exe - Project Version: 3l3a 02-June-2020" << endl;
+		cout << "GIA.exe - Project Version: 3l3b 02-June-2020" << endl;
 		exit(EXIT_OK);
 	}
 
