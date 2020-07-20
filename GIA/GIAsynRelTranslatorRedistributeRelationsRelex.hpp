a530293131e1a1a602db3c587eae2441a9acab46
Richard Baxter <richard.baxter@baxterai.com>
2020-07-20 16:38:19 +1000

    GIA 3l8c - extract GIAposRelTranslatorSANIPropagateOperationsClass::identifySequentialActivationFound/identifyMissingOrVariableEndComponentFound/identifyMissingOrVariableStartComponentFound/identifyMissingStartComponentFound from GIAposRelTranslatorSANIPropagateOperationsClass::propagateWordThroughNetworkGroupVerifyComponentSequenceActivationReady

--- GIA/GIAsynRelTranslatorRedistributeRelationsRelex.hpp
+++ GIA/GIAsynRelTranslatorRedistributeRelationsRelex.hpp
@@ -26,7 +26,7 @@
  * File Name: GIAsynRelTranslatorRedistributeRelationsRelex.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3l8b 15-July-2020
+ * Project Version: 3l8c 15-July-2020
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  * Description: Syntactic Relation Translator - Converts relation objects into GIA nodes (of type entity, action, condition etc) in GIA network/tree
  * /
