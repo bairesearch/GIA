24366a848f6387428798bbee8cd9c4190911f649
Richard Baxter <richard.baxter@baxterai.com>
2020-08-05 15:04:29 +1000

    GIA 3m2b - GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR: fix bug in GIA_POS_REL_TRANSLATOR_SANI_SEMANTIC_NETWORK:!GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_LIMIT_NUM_COMPONENTS:GIA_POS_REL_TRANSLATOR_SANI_SEQUENCE_GRAMMAR_NEVER_SPLIT_GROUP_BETWEEN_TWO_IDENTICAL_COMPONENTS - GIAposRelTranslatorSANIPropagateCompactClass::verifyNeverSplitGroupBetweenTwoIdenticalComponents; compare (lastActivatedComponentSource == firstNonActivatedSource) only if ((lastActivatedComponentSourceSize == 1) && (firstNonActivatedComponentSourceSize == 1))

--- GIA/GIAnlpParser.cpp
+++ GIA/GIAnlpParser.cpp
@@ -26,7 +26,7 @@
  * File Name: GIAnlpParser.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2020 Baxter AI (baxterai.com)
  * Project: General Intelligence Algorithm
- * Project Version: 3m2a 30-July-2020
+ * Project Version: 3m2b 30-July-2020
  * Requirements: requires text parsed by NLP Parser (eg Relex; available in .CFF format <relations>)
  * Description: NLP Parser - Parses tabular subsections (Eg <relations>) of RelEx CFF/Stanford Parser File
  * /
